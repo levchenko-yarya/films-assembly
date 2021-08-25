bffa746c54545b114b9c91a9e762ad6bcb631a31
levchenko-yarya <levchenkoyarya@gmail.com>
2021-08-25 15:25:40 +0300

    crud api

--- api/controller.js
+++ api/controller.js
@@ -1,16 +1,52 @@
 const Movie = require('../movie/model')
 
+// показать все фильмы
 exports.show = (req, res) => {
     Movie.find({}, (err, movies) => {
         if (err) return res.status(400)
-        res.send(movies)
+        return res.send(movies)
     })
 }
 
+// показать фильм по id
 exports.get = (req, res) => {
     return Movie.findById(req.params.id, (err, movie) => {
         if (!err) {
             return res.send(movie)
         }
+    })
+}
+
+// добавить фильм
+exports.store = (req, res) => {
+    const movie = new Movie({
+        title: req.body.title,
+        description: req.body.description,
+        poster: req.body.poster,
+        url: req.body.url,
+        date: req.body.date
+    })
+    movie.save()
+    return res.send('movie is added')
+}
+
+// обновить инфу по фильму
+exports.update = (req, res, next) => {
+    if (!req.params.id) return next(new Error('no Movie id!'))
+    Movie.findByIdAndUpdate(
+        req.params.id,
+        {$set: req.body},
+        (error, movie) => {
+            if (error) return next(error)
+            return res.send('movie is updated')
+        }
+    )
+}
+
+// удалить фильм по id
+exports.delete = (req, res, next) => {
+    Movie.findByIdAndDelete(req.params.id, (error, movie) => {
+        if (!movie) return next(new Error('Movie not found'))
+        return res.send('movie delete')
     })
 }
--- api/router.js
+++ api/router.js
@@ -4,5 +4,8 @@ const router = express.Router()
 
 router.get('/films', controller.show)
 router.get('/film/:id', controller.get)
+router.post('/film/post', controller.store)
+router.put('/film/:id/update', controller.update)
+router.delete('/film/:id/delete', controller.delete)
 
 module.exports = router
