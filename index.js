44363369c2b671e441e8b1821c505a651121d25c
levchenko-yarya <levchenkoyarya@gmail.com>
2021-09-07 10:42:43 +0300

    add graphql

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+
+const {graphqlHTTP} = require('express-graphql')
+const schema = require('./graphql/schema')
+
 const movieRouter = require('./movie/router')
 const apiRouter = require('./api/router')
+
 const app = express()
 
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({extended: false}))
+//app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: schema,
+        graphiql: true
+    })
+)
 app.use('/api', apiRouter)
 app.use('/', movieRouter)
 
