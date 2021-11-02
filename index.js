24791540782d920f8a5f79eb44da6c1e27d378c0
levchenko-yarya <levchenkoyarya@gmail.com>
2021-11-02 19:24:10 +0200

    fix button

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config()
+//require('auth/auth')
 
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require('passport')
 const bodyParser = require('body-parser')
 
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./graphql/schema')
+
+//const UserModel = require('./auth/model')
+//const authRouter = require('./auth/router')
+//const secureRouter = require('./auth/secure-router')
 
 const movieRouter = require('./movie/router')
 const apiRouter = require('./api/router')
@@ -13,7 +19,7 @@ const apiRouter = require('./api/router')
 const app = express()
 
 app.set('view engine', 'hbs')
-//app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.use(
@@ -23,6 +29,9 @@ app.use(
         graphiql: true
     })
 )
+//app.use('/auth', authRouter)
+//app.use('/user', passport.authenticate('jwt', {session: false}), secureRouter)
+
 app.use('/api', apiRouter)
 app.use('/', movieRouter)
 
